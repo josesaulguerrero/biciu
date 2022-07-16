9ee376a3ea984d950bbf0dd9e8177a53ee181d32
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-16 17:24:24 -0500

    Created the files for the entities of the tickets module

---
+++ src/main/java/co/com/biciu/modules/users/persistence/entities/Ticket.java
@@ -0,0 +1,8 @@
+package co.com.biciu.modules.users.persistence.entities;
+
+import co.com.biciu.annotations.Id;
+
+public class Ticket {
+    @Id
+    private String id;
+}
---
+++ src/main/java/co/com/biciu/modules/users/persistence/entities/TicketDate.java
@@ -0,0 +1,4 @@
+package co.com.biciu.modules.users.persistence.entities;
+
+public class TicketDate {
+}
