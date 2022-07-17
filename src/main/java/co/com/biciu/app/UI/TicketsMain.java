10e59fa7f6321be25a3b8eb0d122374e2e034dc5
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-16 21:42:33 -0500

    Implemente the TicketsMain functionality

--- src/main/java/co/com/biciu/app/UI/TicketsMain.java
+++ src/main/java/co/com/biciu/app/UI/TicketsMain.java
@@ -1,4 +1,44 @@
 package co.com.biciu.app.UI;
 
+import co.com.biciu.app.UI.controllers.TicketController;
+import co.com.biciu.utils.UIUtils;
+import org.apache.commons.lang3.Range;
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class TicketsMain {
+    private final TicketController controller;
+    private static TicketsMain instance;
+
+    private TicketsMain() {
+        this.controller = new TicketController();
+    }
+
+    public static TicketsMain getInstance() {
+        if(instance == null) {
+            instance = new TicketsMain();
+        }
+        return instance;
+    }
+
+    public void main() {
+        UIUtils.renderOptionsList(
+                "1. Print all the tickets information.",
+                "2. Pay a ticket."
+        );
+        int selectedOption = UIUtils.readWithValidatorAndParser(
+                value -> NumberUtils.isParsable(value) && Range.between(1, 2).contains(Integer.parseInt(value)),
+                value -> Integer.parseInt(value.trim())
+        );
+        callController(selectedOption);
+    }
+
+    private void callController(int option) {
+        switch (option) {
+            case 1:
+                controller.printAll();
+                break;
+            case 2:
+                controller.pay();
+        }
+    }
 }
