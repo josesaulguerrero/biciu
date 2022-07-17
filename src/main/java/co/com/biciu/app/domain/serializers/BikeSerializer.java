05ae268fc163efc5a0dd051a44f2adcb54eb9ccd
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-17 03:15:58 -0500

    Created the BikeSerializer

---
+++ src/main/java/co/com/biciu/app/domain/serializers/BikeSerializer.java
@@ -0,0 +1,26 @@
+package co.com.biciu.app.domain.serializers;
+
+import co.com.biciu.app.persistence.entities.Bike;
+import co.com.biciu.app.persistence.entities.BikeType;
+import co.com.biciu.interfaces.Serializer;
+
+public class BikeSerializer implements Serializer<Bike, String> {
+    @Override
+    public String serialize(Bike entity) {
+        return entity.getId() + ";" +
+                entity.getColor() + ";" +
+                entity.isAvailable() + ";" +
+                entity.getType().name() + ";";
+    }
+
+    @Override
+    public Bike deserialize(String serializedObject) {
+        String[] properties = serializedObject.split(";");
+        return new Bike(
+                properties[0],
+                properties[1],
+                Boolean.valueOf(properties[2]),
+                BikeType.valueOf(properties[3])
+        );
+    }
+}
